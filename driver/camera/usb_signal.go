6609cb29163f148cf57543ef5a1a5e4d289b8063
JK-97 <benjamin-dream@outlook.com>
2019-11-05 12:42:33 +0800

    fix framesizes && signal check on start

--- driver/camera/usb_signal.go
+++ driver/camera/usb_signal.go
@@ -4,29 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
-
-	"github.com/reiver/go-v4l2"
 )
 
-func EnsureUsbExists(address string) error {
-	if strings.HasPrefix(address, "rtsp") {
-		return nil
-	}
-
-	device, err := v4l2.Open(address)
-	if nil != err {
-		return err
-	}
-	defer device.Close()
-
-	busInfo, err := device.BusInfo()
-	if err != nil {
-		return err
-	}
-
-	busInfos := strings.Split(busInfo, "-")
-	valuePath := fmt.Sprintf("/sys/devices/platform/%v/usb5/5-1/5-%v/bConfigurationValue", busInfos[1], busInfos[2])
+// usb eg. 1.2, 1.3
+func EnsureUsbExists(usb string) error {
+	valuePath := fmt.Sprintf("/sys/devices/platform/fe380000.usb/usb5/5-1/5-%v/bConfigurationValue", usb)
 	_, err = os.Stat(valuePath)
 	if err != nil {
 		return err
