e5f77a3c5d799cb91f59b5cd04f17b17eec8e0f4
JK-97 <benjamin-dream@outlook.com>
2019-11-05 12:44:39 +0800

    fix framesizes && signal check on start

--- driver/camera/usb_signal.go
+++ driver/camera/usb_signal.go
@@ -9,7 +9,7 @@ import (
 // usb eg. 1.2, 1.3
 func EnsureUsbExists(usb string) error {
 	valuePath := fmt.Sprintf("/sys/devices/platform/fe380000.usb/usb5/5-1/5-%v/bConfigurationValue", usb)
-	_, err = os.Stat(valuePath)
+	_, err := os.Stat(valuePath)
 	if err != nil {
 		return err
 	}
